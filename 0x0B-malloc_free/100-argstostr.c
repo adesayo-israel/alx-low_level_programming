a634782e24629bad92499e64353261e3e314991b
Adesayo Israel <adesayobukunmiisrael@gmail.com>
2023-11-01 12:11:00 -0700

    commit 6 project 7

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+/**
+ * argstostr - this functiion basically concatenates
+ * all the arguments
+ *
+ * @ac: this represents the argument counter
+ * @av: this represents the argument holder
+ *
+ * Return: new string
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, length;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	length = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+		length++;
+	}
+
+	str = malloc((length + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
+}
+
