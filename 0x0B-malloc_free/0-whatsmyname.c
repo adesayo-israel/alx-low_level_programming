0e911a4641dd5dc50f1d6b88e95393cc853773f6
Adesayo Israel <adesayobukunmiisrael@gmail.com>
2023-11-01 17:17:51 -0700

    project 8 commit 1

---
+++ 0x0B-malloc_free/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- prints the program's name followed by a new line
+ *
+ * @argc: contains the number of arguments passed to the program
+ *
+ * @argv: it is a one dimentional array of strings
+ *
+ * Return: 0.
+ *
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
+
