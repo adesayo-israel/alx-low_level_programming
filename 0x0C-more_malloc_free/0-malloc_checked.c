85deb672c6535768a6147b2e05f3d63fbcdbda67
Adesayo Israel <adesayobukunmiisrael@gmail.com>
2023-11-02 13:58:14 -0700

    project 1 commit 1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ *
+ * @b: holds value for number of bytes allocated
+ *
+ * Return: poiter to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,10 @@
+0x0C. C - More malloc, free
+C
+Memory allocation
+
+Concepts
+For this project, we expect you to look at this concept:
+Automatic and dynamic allocation, malloc and free
+
+How to use the exit function
+	What are the functions calloc and realloc from the standard library and how to use them
