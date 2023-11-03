25ba39c41575c94e8e90fa78ff904b2360a4dae0
Adesayo Israel <adesayobukunmiisrael@gmail.com>
2023-11-02 19:59:46 -0700

    project 2 commit 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - a function that concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * @n: size of bytes
+ * Return: newly allocated space in memory
+ * If the function fails, it should return NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	unsigned int len1 = strlen(s1);
+	unsigned int len2 = strlen(s2);
+
+	if (n >= len2)
+		n = len2;
+
+	char *rest = malloc(len1 + n + 1);
+
+	if (rest == NULL)
+		return (NULL);
+
+	char *dest = rest;
+
+	while (*s1)
+	{
+		*dest = *s1;
+		dest++;
+		s1++;
+	}
+
+	while (n > 0)
+	{
+		*dest = *s2;
+		dest++;
+		s2++;
+		n--;
+	}
+
+	*dest = '\0';
+	return (rest);
+}
